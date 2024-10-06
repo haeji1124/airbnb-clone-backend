392953b8bb4ce7fbd73e6cf69d438431fba7c9f9
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2024-10-06 21:08:39 +0900

    previous solution

--- tweets/views.py
+++ tweets/views.py
@@ -5,7 +5,11 @@ from .models import Tweet
 
 
 @api_view(["GET"])
-def all_tweets(request):
-    tweets = Tweet.objects.all()
+def tweets_by_user(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        raise NotFound
+    tweets = Tweet.objects.filter(user=user)
     serializer = TweetSerializer(tweets, many=True)
     return Response(serializer.data)
--- users/views.py
+++ users/views.py
@@ -8,9 +8,9 @@ from tweets.serializers import TweetSerializer
 @api_view(["GET"])
 def tweets_by_user(request, pk):
     try:
-      user = User.objects.get(pk=pk)
+        user = User.objects.get(pk=pk)
     except User.DoesNotExist:
-      raise NotFound
+        raise NotFound
     tweets = Tweet.objects.filter(user=user)
     serializer = TweetSerializer(tweets, many=True)
     return Response(serializer.data)
